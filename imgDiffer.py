849cbb511523aedbec0daf3970300bc7c6aab3c2
donythepony <donythepony@gmail.com>
2020-01-16 15:27:00 +0100

    Added Commandline Args

--- imgDiffer.py
+++ imgDiffer.py
@@ -1,8 +1,42 @@
 from PIL import Image
 import numpy as np
+import sys
+import argparse
+
+CLI = argparse.ArgumentParser()
+
+CLI.add_argument(
+        "--targetColor",
+        nargs="*",
+        type=int,
+        default=(255, 0, 0, 255)
+)
+
+CLI.add_argument(                                                                                                                                                                                                          "--destinationColor",
+        nargs="*",                                                                                                                                                                                                         type=int,
+        default=(0, 0, 0, 255)
+)
+
+CLI.add_argument(
+        "--imagePath",
+        nargs="*",
+        type=str,
+        default="image.png"
+)
+
+args = CLI.parse_args()
+
+#Get target und destination color
+targetColor = args.targetColor
+destinationColor = args.destinationColor
+
+print(args.targetColor)
+
+#Get image path
+imagePath = args.imagePath
 
 #Load mage and extracting data
-imageA = Image.open("image.png")
+imageA = Image.open(imagePath)
 imageAPixels = imageA.load()
 width, height = imageA.size
 
@@ -14,10 +48,10 @@ newData = np.zeros((height, width, 4))
 for x in range(width):
     for y in range(height):
         currentPixel = data[y, x]
-        if (data[y, x] == (237, 27, 36, 255)).all():
-            currentPixel= [78, 95, 191, 255]
+        if (data[y, x] == targetColor).all():
+            currentPixel= destinationColor
         newData[y, x] = currentPixel
 
 #save new image
 newImg = Image.fromarray(newData.astype('uint8'), 'RGBA')
-newImg.save('newImg.png')
+newImg.save("processedImage.png")
